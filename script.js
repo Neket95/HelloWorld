710d7125540531913877c2b72313df0122528ade
Neket95 <Logan-nik@yandex.ru>
2023-07-22 12:02:50 +0500

    hw-N+4

--- script.js
+++ script.js
@@ -28,23 +28,74 @@ while (b <= n) {
 // задание 4. 
 
 const obj = {
-    name : 'Коля',
-    solary : 200,
+  name : "Коля", salary : 200,
+  name : "Вася", salary : 300,
+  name : "Петя", salary : 400,
+
 }
 
-for (let description in obj) {
-
+for (let name in obj) {
+    console.log(`${obj.name} — зарплата ${obj.salary} долларов.`);
 }
     
 // задание 5.
 
+let a = 1000;
+let num = 0;
+while (a > 50) {
+  a /= 2;
+  num++;
+}
+ 
+console.log(a);
+console.log(num);
+
+// задание 6. (вариант 1)
 
 
+let dayNumber = +prompt('Введите число первой пятницы месяца');
 
-// задание 6.
-
-
+while (dayNumber <= 31) {
+    if (1 <= dayNumber && dayNumber <= 3) {
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+    }
+    else if (4 <= dayNumber && dayNumber <= 7) {
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+        dayNumber = dayNumber + 7;
+        console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+    }
     
+    else {
+        console.log(`На сегодня нет задач.`);
+    }
+
+    break;
+}
+
+
+ // Задание 6 (вариант 2)
+
+
+let dayNumber2 = +prompt('Введите число первой пятницы месяца');
+for ( ; dayNumber2 <= 31; dayNumber2+=7) { 
+	if (dayNumber2 % 5 == 0) { 
+		continue; 
+	}
+	console.log(`Сегодня пятница, ${dayNumber2}-е число. Необходимо подготовить отчет.`);
+}
+
 
 
 
